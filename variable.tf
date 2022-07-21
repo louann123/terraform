17c8234fb57bd9f847de0896e5090231d33ce1da
Louis <louisbabila@yahoo.com>
2022-07-21 05:09:14 -0500

    terrafor-code

---
+++ variable.tf
@@ -0,0 +1,21 @@
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "ami" {
+  type    = string
+  default = "ami-0cff7528ff583bf9a"
+}
+
+variable "instance-type" {
+  type    = string
+  default = "t2.nano"
+}
+
+variable "keypair" {
+  type    = string
+  default = "Key1"
+}
+
+
